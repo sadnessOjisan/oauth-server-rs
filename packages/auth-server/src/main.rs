7d3dba16287da000747e1e653e711e60faa4f95c
sadness_ojisan <sadness.ojisan@gmail.com>
2022-08-23 02:34:22 +0900

    Add authorization page

--- packages/auth-server/src/main.rs
+++ packages/auth-server/src/main.rs
@@ -1,3 +1,49 @@
-fn main() {
-    println!("Hello, world!");
+use askama::Template;
+use axum::{
+    http::StatusCode,
+    response::{Html, IntoResponse, Response},
+    routing::get,
+    Router,
+};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    // build our application with some routes
+    let app = Router::new()
+        .route("/authorization", get(authorization));
+
+    // run it
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn authorization() -> impl IntoResponse {
+    let template = AuthorizationTemplate;
+    HtmlTemplate(template)
+}
+
+#[derive(Template)]
+#[template(path = "authorization.html")]
+struct AuthorizationTemplate;
+
+struct HtmlTemplate<T>(T);
+
+impl<T> IntoResponse for HtmlTemplate<T>
+where
+    T: Template,
+{
+    fn into_response(self) -> Response {
+        match self.0.render() {
+            Ok(html) => Html(html).into_response(),
+            Err(err) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Failed to render template. Error: {}", err),
+            )
+                .into_response(),
+        }
+    }
 }
