66fe3a62b748db69ea753d9b33e37dfc534d6655
sadness_ojisan <sadness.ojisan@gmail.com>
2022-08-28 13:11:33 +0900

    Redirect if pass is wrong

--- packages/auth-server/src/main.rs
+++ packages/auth-server/src/main.rs
@@ -5,7 +5,8 @@ use axum::{
     routing::get,
     Extension, Router,
 };
-use std::{collections::HashMap, iter::Map, net::SocketAddr};
+use std::{collections::HashMap, iter::Map, net::SocketAddr, sync::Arc};
+use tokio::sync::{Mutex, RwLock};
 
 #[derive(Clone)]
 struct UserId(u32);
@@ -23,13 +24,27 @@ struct User {
 #[derive(Clone)]
 struct UserEmailMap(HashMap<UserEmail, User>);
 
+/// 認可コード
+#[derive(Eq, Hash, PartialEq, Clone)]
 struct AuthorizationCode(String);
 
+#[derive(Clone)]
 struct AbleableAuthorizationCodeMap(HashMap<AuthorizationCode, User>);
 
+#[derive(Clone)]
 struct AccessToken(String);
 
+#[derive(Clone)]
 struct AbleableAccessTokenMap(HashMap<AccessToken, User>);
+
+#[derive(Clone)]
+struct Store {
+    userEmailMap: UserEmailMap,
+    ableableAuthorizationCodeMap: AbleableAuthorizationCodeMap,
+    ableableAccessTokenMap: AbleableAccessTokenMap,
+}
+
+type SharedStore = Arc<Mutex<Store>>;
 
 #[tokio::main]
 async fn main() {
@@ -42,12 +57,18 @@ async fn main() {
         password: "sadness_ojisan".to_string(),
     };
     users.0.insert(user_email_1, user_1);
+    let store: SharedStore = Arc::new(Mutex::new(Store {
+        userEmailMap: users,
+        ableableAccessTokenMap: AbleableAccessTokenMap(HashMap::new()),
+        ableableAuthorizationCodeMap: AbleableAuthorizationCodeMap(HashMap::new()),
+    }));
     println!("start server");
     // build our application with some routes
     let app = Router::new()
         .route("/authorization", get(authorization))
         .route("/decide_authorization", get(decide_authorization))
-        .layer(Extension(users));
+        // グローバルなstoreの代わりとして。
+        .layer(Extension(store));
 
     // run it
     let addr = SocketAddr::from(([127, 0, 0, 1], 3001));
@@ -64,20 +85,27 @@ async fn authorization() -> impl IntoResponse {
 }
 
 /// メアド、パスワードを受け取って、そのユーザー用の token を作る。
-async fn decide_authorization(users: Extension<UserEmailMap>) -> impl IntoResponse {
+async fn decide_authorization(store: Extension<SharedStore>) -> impl IntoResponse {
     let requested_email = "sadness_ojisan@example.com".to_string();
     let requested_pass = "sadness_ojisan";
     let requested_user_email = UserEmail(requested_email);
 
-    let got_user: Option<&User> = users.0.0.get(&requested_user_email);
-    let user = got_user.unwrap();
+    let mut locked_store = store.0.try_lock().unwrap();
+    let got_user = &locked_store.userEmailMap.0.get(&requested_user_email);
+    let user = got_user.unwrap().clone();
     let user_pass = user.password.as_str();
 
-    // if 文のなかから redirect できないんだっけ？
     let redirected = if requested_pass == user_pass {
         // 普通はあらかじめ権限リクエストするアプリを作った人がどこにリダイレクトさせておきたいかを登録している想定
         let redirect_url = "http://localhost:3000/redirected";
-        let code = "this_is_ninka_code";
+        let code = format!("this_is_ninka_code_of_user_id_{}", &user.id.0);
+
+        // drop できなくてここのロックを剥がせない。どうすれば？
+        locked_store
+            .ableableAuthorizationCodeMap
+            .0
+            .insert(AuthorizationCode(code.clone()), user.clone());
+
         let formated = format!("{}?code={}", redirect_url, code);
         let path = formated.as_str();
         Redirect::temporary(path)
